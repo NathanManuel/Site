8fa480349fcfb59b582680d48f7dc824ef1d048f
Nathan van Velden <nathanvanvelden@gmail.com>
2021-03-16 20:25:26 +0200

    Add project files.

---
+++ Site/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SitesController : ControllerBase
+    {
+
+
+        private static List<Models.Movie> movies = new List<Models.Movie>()
+        {
+           new Models.Movie() { ID = Guid.NewGuid(), MoveiL = 140, MovieName= "Thor" },
+           new Models.Movie() { ID = Guid.NewGuid(), MoveiL = 120, MovieName= "Captin America" },
+           new Models.Movie() { ID = Guid.NewGuid(), MoveiL = 200, MovieName= "Star Wars" }
+        };
+
+        [HttpGet]
+        public Models.Movie[] Get()
+        {
+            return movies.ToArray();
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] Models.Movie movie)
+        {
+            if (movie.ID == Guid.Empty)
+                movie.ID = Guid.NewGuid();
+
+            movies.Add(movie);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Models.Movie movie)
+        {
+            Models.Movie currnetMovie = movies.FirstOrDefault(x => x.ID == movie.ID);
+            currnetMovie.MovieName = currnetMovie.MovieName;
+            currnetMovie.MoveiL = currnetMovie.MoveiL;
+        }
+
+        [HttpDelete]
+        public void Remove(Guid id)
+        {
+            movies.RemoveAll(movie => movie.ID == id);
+        }
+
+
+    }
+}
---
+++ Site/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string MovieName { get; set; }
+        public int MoveiL { get; set; }
+    }
+}
